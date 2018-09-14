c045e53890290e1530166a874f9c803a5e5a7907
Paul Bauer <paul.bauer@datadoghq.com>
2018-09-14 17:58:46 -0400

    code review comments

--- csvAutoFormat.py
+++ csvAutoFormat.py
@@ -5,12 +5,25 @@ import csv
 import os
 import smtplib
 
+### Suggest using the argparse package and passing in credentials
+### it's easy to forget and accidentally check in your creds and push to git, just as you did here ;)
+### I would change your password asap
+###
+### e.g.
+### import argparse
+### parser = argparse.ArgumentParser(description=__doc__)
+### parser.add_argument("user", help="gmail user")
+### args = parser.parse_args()
+### gmail_user = args.user
+
 #define sender's Google authentication info
 gmail_user = "holden@newlifescientific.com"
 gmail_password = "33walrusesandrats1999"
 
 #Name & path of CSV file. File must be in the same directory as the script, and must be named list.txt. Janky, I know. I'm going to fix that.
 file_dir = os.path.dirname(os.path.realpath('__file__'))
+
+### this could also be a cmd line param
 file_name = "list.txt"
 
 #opens file in read-mode, assigns opened file to variable 'csvfile'
@@ -46,6 +59,11 @@ def send_email(x):
     server.sendmail(gmail_user, recipient, email_text)
     print(email_text)
 
+### Recursion is fun, but also can cause issues
+### For example, the number of calls to send_email and stack frames created is dependent on
+### the number of rows in list.txt; too many and it will blow the stack.
+### I would put the iteration outside send_email.
+    
     #This iterates to the next row in the CSV file
     row_to_read = next(entry_reader)
 
@@ -53,6 +71,8 @@ def send_email(x):
         send_email(row_to_read)
     else:
         print("All emails sent.")
+### If you put the control flow outside send_email, the program execution is clearer and you don't need an exit here
+### inside send_email
         exit()
 
 #If connection to server is made, send_email() method is run. Otherwise, an authentication exception is raised.
@@ -68,5 +88,6 @@ except smtplib.SMTPAuthenticationError:
 #After script has run (successfully or unsuccessfully), prompt user to exit
 finally:
     print("Exiting...")
+# is there a reason we are explicitly exiting?
     exit()
 
