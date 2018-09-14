a5b18af2a86ea26be2cae31c2457777daa13e3c3
TheGoulashGuy <holden.galusha@gmail.com>
2018-09-13 22:23:52 -0400

    cleaned up code

--- csvAutoFormat.py
+++ csvAutoFormat.py
@@ -4,46 +4,52 @@
 import csv
 import os
 import smtplib
-#import timestampScraper
 
 #define sender's Google authentication info
-gmail_user = "sender@example.com"
+gmail_user = "user@example.com"
 gmail_password = "password"
 
-fileDir = os.path.dirname(os.path.realpath('__file__'))
+#Name & path of CSV file. File must be in the same directory as the script, and must be named list.txt. Janky, I know. I'm going to fix that.
+file_dir = os.path.dirname(os.path.realpath('__file__'))
+file_name = "list.txt"
 
-#name of CSV file. Due to above line, no filepath needs defined
-filename = "today.txt"
-
-#opens file in read-mode, assigns opened file to variabel 'csvfile'
-csvfile = open(filename, "r")
+#opens file in read-mode, assigns opened file to variable 'csvfile'
+csv_file = open(file_name, "r")
 
 #initializes variable that calls reader function from csv module
-entryReader = csv.reader(csvfile, delimiter="," )
-0
-#prints every element in line (which is a list) on a new line
-row_to_read = next(entryReader)
+entry_reader = csv.reader(csv_file, delimiter="," )
 
+#prints every element in line (which is a list) on a new line
+row_to_read = next(entry_reader)
 
 #Extracts info form row_to_read list object, inserts it into body, and sends the info to the specified ("to") address, then closes the server.
 def send_email(x):
-    sent_from = "sender@example.com"
-    to = "recipient@example.com"
-    subject = str(x[1])
-    body = "\nTITLE: " + x[1] + "\n\nURL: https://www.ebay.com/itm/" + x[0] + "\n\nPRICE: $" + str(x[3]) + "\n\nVENDOR: " + x[4] + "\n\nENDING DATE: " + str(x[6])
-    email_text = """
-    From: {}
-    To: {}
+    recipient = "recipient@example.com"
+    #body = "\nTITLE: " + x[1] + "\n\nURL: https://www.ebay.com/itm/" + x[0] + "\n\nPRICE: $" + str(x[3]) + "\n\nVENDOR: " + x[4] + "\n\nENDING DATE: " + str(x[6])
+    email_text = (f"""
+    From: {gmail_user}
+    To: {recipient}
     Subject: --Watched Item--
-
-    {}
+    
+    TITLE: {x[1]}
+    
+    URL: https://www.ebay.com/itm/{x[0]}
+    
+    PRICE: ${x[2]}
+    
+    VENDOR: {x[3]}
+    
+    ENDING DATE: {x[5]}
 
     \nThis is an automated message.
-    """.format(sent_from, to, body)
-    server.sendmail(sent_from, to, email_text)
-    print(body)
+    """)
+    #.format(gmail_user, recipient, body)
+
+    server.sendmail(gmail_user, recipient, email_text)
+    print(email_text)
+
     #This iterates to the next row in the CSV file
-    row_to_read = next(entryReader)
+    row_to_read = next(entry_reader)
     try:
         #Recursive method call using above 'row_to_read' variable as argument
         send_email(row_to_read)
@@ -60,5 +66,4 @@ try:
     send_email(row_to_read) #Initial send_email() function call
 
 except:
-    print("ERROR: Could not send email. Check formatting of input file and network connection.")
-
+   print("ERROR: Could not send email. Check formatting of input file and/or network connection.")
